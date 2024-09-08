30e7411eb3a637bd5ec3856bcd69b4ae34976b2b
Abd-alkareem <abdalkareem.of@gmail.com>
2024-09-07 17:46:02 -0700

    modefide

--- js/main.js
+++ js/main.js
@@ -108,7 +108,7 @@ let data = [
     {
         projectName:"to do list",
         projectURL:"https://abd-alkareem.github.io/to-do-list/",
-        cardImage:"./image/sites/to-do-003",
+        cardImage:"./image/sites/to-do-003.png",
         projectRepo:"https://github.com/Abd-alkareem/to-do-list",
     },
 ];
